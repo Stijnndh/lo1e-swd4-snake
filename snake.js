cb00d4a4f2b9bc499c8b7adcca6f080262dad34d
Stijnndh <6019147@mborijnland.nl>
2022-09-13 13:38:38 +0200

    Snake move right

--- snake.js
+++ snake.js
@@ -1,15 +1,29 @@
 let canvas = document.getElementById("snake");
 let ctx = canvas.getContext("2d");
 
+let snakeX = 200;
+let snakeY = 200;
+
+function drawBackground() {
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 400, 400);
-
-ctx.fillStyle = "green";
-ctx.fillRect(200, 200, 20, 20)
-
-function update() {
-    console.log('update');
 }
 
+
+function drawSnake(){
+ctx.fillStyle = "green";
+ctx.fillRect(snakeX, snakeY, 20, 20)
+}
+
+function update() {
+    snakeX += 20;
+
+    drawBackground();
+    drawSnake();
+}
+
+
+drawBackground();
+drawSnake();
 
 setInterval(update, 1000);
